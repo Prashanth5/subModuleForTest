4eccb19b4fc30ca11266a4ba3a795141699cbdeb
Prashanth <pparate2@ballytech.com>
2018-02-19 16:18:23 +0530

    Changes

---
+++ subModuleLib/subModuleLib/color.h
@@ -0,0 +1,15 @@
+//
+//  color.h
+//  subModuleLib
+//
+//  Created by Prashanth on 2/19/18.
+//  Copyright © 2018 com.pkp. All rights reserved.
+//
+
+#ifndef color_h
+#define color_h
+
+#import UIKIT
+
+#endif /* color_h */
+
---
+++ subModuleLib/subModuleLib/subModuleLib.h
@@ -0,0 +1,19 @@
+//
+//  subModuleLib.h
+//  subModuleLib
+//
+//  Created by Prashanth on 2/19/18.
+//  Copyright © 2018 com.pkp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for subModuleLib.
+FOUNDATION_EXPORT double subModuleLibVersionNumber;
+
+//! Project version string for subModuleLib.
+FOUNDATION_EXPORT const unsigned char subModuleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <subModuleLib/PublicHeader.h>
+
+
